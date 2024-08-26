4b779f885e1f3977cef0bfd2fbf254ea5a42e4d8
FouTY <gabrielcmarchetti@gmail.com>
2024-08-25 21:45:48 -0300

    Desenvolvendo lab MC404

--- lab_03/main.c
+++ lab_03/main.c
@@ -1,11 +1,12 @@
 #define STDIN_FD 0
 #define STDOUT_FD 1
+typedef enum {false, true} bool;
 
 #ifdef LOCALEXEC
 #include <unistd.h>
 #include <stdlib.h>
+#include <stdio.h>
 #else
-
 int read(int __fd, const void *__buf, int __n){
     int ret_val;
   __asm__ __volatile__(
@@ -48,83 +49,97 @@ void exit(int code)
   );
 }
 
+// Function declaration.
+int main();
+int abs(int num);
+
+
+
 void _start(){
   int ret_code = main();
   exit(ret_code);
 }
 #endif
 
-typedef enum {false, true} bool;
-
-// Function declaration.
-int main();
-int abs(int num);
 
 int abs(int num){
   if( num < 0 ) return num * -1;
   return num;
 }
 
-char digitToChar(int digit){
-  return (char) digit + 48; 
+int readDecimal(char buf[20], int n){
+  int tmp = 0;
+  int base = 1;
+  int lsd_pos = n - 1; // Least-Significant Bit position;
+  while( true ){
+    if(lsd_pos < 0)
+      break;
+    if(buf[lsd_pos] >= '0' && buf[lsd_pos] <= '9'){
+      tmp += (buf[lsd_pos] - '0') * base;
+      base *= 10;
+    }
+    lsd_pos--;
+  } 
+ 
+  return tmp;
 }
 
-char table[] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'};
-int convertHexToDec(char *buf, int n){
-  int storedValue = 0;
+void printBuffer(char buf[20], int n){
+  for(int i = 0; i < n; ++i){
+    printf("%c", buf[i]);
+  }
+  printf("\n");
+}
+
+int readHexadecimal(char buf[20], int n){
+  int tmp = 0;
+  int lsd_pos = n-1;
   int base = 1;
 
-  int k = 0;
-  for(int i = n - 2; i >= 0; --i){
-    for(k = 0; k < 16; ++k){
-      if(buf[i] == table[k]) break;
-    } 
-
-    storedValue += base * k;
-    base *= 16;
-  }
-
-  return storedValue;
-}
-
-int convertDecStrToDec(char *buf, int n){
-  int storedValue = 0;
-  int base = 1;
-
-  int k = 0;
-  for(int i = n - 2; i >= 0; --i){
-    for(k = 0; k < 10; ++k){
-      if(buf[i] == table[k]) break;
-    } 
-
-    storedValue += base * k;
-    base *= 10;
-  }
-
-  return storedValue;
-}
-
-void convertDecToC2(int readValue, char *globalBuffer){
-  char buf[32 + 3]; // +3 due to '\n', '0b';
-  buf[0] = '0';
-  buf[1] = 'b';
-  buf[2] = (readValue < 0) ? '1': '0'; 
-  buf[32 + 3 - 1] = '\n'; // -1 due to 0-index, note that 32 + 3 equals total size.
-  int absValue = abs(readValue);
-  int currentIndex = 32 - 1; // This is the LSB.
-
-  int remainder = 0; 
-  int quotient = (readValue < 0) ? absValue - 1 : absValue;
+  //printBuffer(buf, n);
   while(true){
-    remainder = readValue % 2;
-    buf[currentIndex] = digitToChar(remainder);
-    currentIndex--;  
-    quotient = quotient / 2;
-    if(quotient == 0) break;
+    if(lsd_pos < 0)
+      break;
+    if(buf[lsd_pos] >= '0' && buf[lsd_pos] <= '9'){
+      tmp += (buf[lsd_pos] - '0') * base;
+      base *= 16;
+    }
+    if(buf[lsd_pos] >= 'A' && buf[lsd_pos] <= 'E'){
+      tmp += (10 + buf[lsd_pos] - 'A') * base;
+      base *= 16;
+    }
+    lsd_pos--;
   }
 
-  globalBuffer = buf;
-} 
+  return tmp;
+}
+
+void store_two_complement(char buf[32+3], int readValue){
+  
+}
+
+void store_decimal(char *buf, int readValue){
+  char aux_buf[20];
+  int buf_index = 0;
+  if(readValue < 0){
+    buf[0] = '-';
+    buf_index = 1;
+  }
+  readValue = abs(readValue);
+
+  int size_num = 0;
+  while(readValue){
+    aux_buf[size_num] = readValue % 10;
+    readValue /= 10;
+    size_num++;
+  }
+  
+  while(size_num){
+    buf[buf_index] = aux_buf[size_num];
+    size_num--;
+    buf_index++;
+  }
+}
 
 int main(){
   char buf[20];
@@ -136,21 +151,22 @@ int main(){
   int readValue = 0;
   switch (isHexadecimal) {
     case false:
-      readValue = convertHexToDec(buf, n);
+      readValue = readDecimal(buf, n); 
+      if(isNegative) readValue *= -1;
       break;
     case true:
-      readValue = convertDecStrToDec(buf, n);
-      if(isNegative) readValue *= -1;
+      readValue = readHexadecimal(buf, n);
       break;
     default:
       break;
   } 
-  
-  char *c2_number, *dec_number, *hex_number, *unsigned_number;
-  convertDecToC2(readValue, c2_number);
-  write(STDOUT_FD, c2_number, n);
+  char two_complement_buf[32+3]; // +3 Due to "Ob" at the beggining and "\n" at the end
+  char decimal_buf[20]; // 20 is enough
+  char hexadecimal_buf[8+3];
+  char decimal_buf_swapped_endianess[20];
 
-  write(STDOUT_FD, buf, n);
+  printf("%d\n", readValue);
+
 
   return 0;
 }
