f882d61b5c8ec53efe898326716fe3e2f93d20a6
FouTY <g251055@dac.unicamp.br>
2024-08-26 11:14:45 -0300

    Consegui implementar as parter 1, 2 e 3 do lab. Falta só fazer o último item

--- lab_03/main.c
+++ lab_03/main.c
@@ -86,9 +86,8 @@ int readDecimal(char buf[20], int n){
 
 void printBuffer(char buf[20], int n){
   for(int i = 0; i < n; ++i){
-    printf("%c", buf[i]);
+    printf("%2c ", buf[i]);
   }
-  printf("\n");
 }
 
 int readHexadecimal(char buf[20], int n){
@@ -114,31 +113,140 @@ int readHexadecimal(char buf[20], int n){
   return tmp;
 }
 
-void store_two_complement(char buf[32+3], int readValue){
+int store_two_complement(char buf[32+3], int readValue){
+  buf[0] = '0'; buf[1] = 'b';
+  if(readValue == 0){
+    buf[2] = '0';
+    buf[3] = '\n';
+    return 4;
+  }
+
+  unsigned int aux_binary_number;
+  if(readValue < 0){
+    aux_binary_number = ~(abs(readValue)) + 1;
+  }
+  else{
+    aux_binary_number = readValue;
+  }
+
+  char aux_buf[32];
+  int current_bit = 0, aux_buf_index = 0, buf_index = 2;
+  while(aux_binary_number){
+    current_bit = (aux_binary_number & 1) + '0';
+    aux_buf[aux_buf_index++] = current_bit;
+    aux_binary_number >>= 1;
+  } 
   
+  while(aux_buf_index){
+    buf[buf_index++] = aux_buf[--aux_buf_index];
+  }
+  buf[buf_index++] = '\n';
+
+  return buf_index;
 }
 
-void store_decimal(char *buf, int readValue){
+int store_decimal(char *buf, int readValue){
   char aux_buf[20];
   int buf_index = 0;
+  if(readValue == 0){
+    buf[0] = '0';
+    buf[1] = '\n';
+    return 1;
+  }
   if(readValue < 0){
     buf[0] = '-';
     buf_index = 1;
   }
-  readValue = abs(readValue);
 
   int size_num = 0;
-  while(readValue){
-    aux_buf[size_num] = readValue % 10;
-    readValue /= 10;
-    size_num++;
+  unsigned int aux_readValue = abs(readValue);
+  while(aux_readValue){
+    aux_buf[size_num++] = aux_readValue % 10;
+    aux_readValue /= 10;
   }
   
   while(size_num){
-    buf[buf_index] = aux_buf[size_num];
-    size_num--;
-    buf_index++;
+    buf[buf_index++] = aux_buf[--size_num] + '0';
   }
+  buf[buf_index++] = '\n';
+
+  return buf_index; 
+}
+
+int store_hexadecimal(char *hex_buf, char *bin_buf, int bin_size){
+  int counter = 0;
+  int value_hex_bit = 0, hex_buf_index = 2, aux_hex_buf_index = 0, base = 1;
+  char aux_hex_buf[8];
+
+  hex_buf[0] = '0'; hex_buf[1] = 'x';
+  //printf("------------------------------\n");
+  //for(int i = 0; i < bin_size - 1; i++)
+  //  printf("%2d ", i);
+  //printf("\n");
+  //printBuffer(bin_buf, bin_size);
+
+  //printf("bin_size: %d\n", bin_size);
+  for(int i = bin_size - 2; i >= 2; i--){
+    value_hex_bit += (bin_buf[i] - '0') * base; 
+    base *= 2;
+
+    if( (counter+1) % 4 == 0 ){
+      aux_hex_buf[aux_hex_buf_index++] = value_hex_bit;
+      value_hex_bit = 0;
+      base = 1;
+    }
+    counter++;
+  } 
+  if(counter % 4 != 0){
+    aux_hex_buf[aux_hex_buf_index++] = value_hex_bit; 
+  }
+
+  while(aux_hex_buf_index)
+    hex_buf[hex_buf_index++] = aux_hex_buf[--aux_hex_buf_index];
+
+  // Convert to ASCII.
+  int value;
+  for(int i = 2; i < hex_buf_index; i++){
+    value = hex_buf[i]; 
+    if( value >= 0 && value <= 9 )
+      hex_buf[i]+= '0';
+    if( value >= 10 && value <= 15){
+      hex_buf[i] = hex_buf[i] - 10 + 'a';
+    }
+  }
+  
+  hex_buf[hex_buf_index++] = '\n';
+  return hex_buf_index;
+}
+
+int store_swap_endianess(char *swap_end_buf, char *bin_buf, int bin_size){
+  char aux_bin_buf[32];
+  int aux_index = 0, size_aux_buf;
+  //for(int i = 0; i < bin_size - 1; i++)
+  //  printf("%2d ", i);
+  //printf("\n");
+  //printBuffer(bin_buf, bin_size);
+  //printf("bin_size: %d\n", bin_size);
+
+  size_aux_buf = 0;
+  for(int i = bin_size - 2; i >= 2; i--){
+    aux_bin_buf[aux_index++] = bin_buf[i];
+    size_aux_buf++;
+  } 
+  //printf("%d\n", size_aux_buf);
+  //printBuffer(aux_bin_buf, bin_size - 1);
+  
+  unsigned int uns_decimal = 0;
+  int group = 1;
+  for(int i = 0; i < size_aux_buf; i++){
+    if( i % 4 == 0 )
+      group++;
+    uns_decimal += (aux_bin_buf[i] - '0') << ( group * 4 - ((i % 4) + 1) );
+  }
+  printf("%d\n", uns_decimal);
+
+
+  return 0;
 }
 
 int main(){
@@ -160,12 +268,23 @@ int main(){
     default:
       break;
   } 
+
   char two_complement_buf[32+3]; // +3 Due to "Ob" at the beggining and "\n" at the end
   char decimal_buf[20]; // 20 is enough
   char hexadecimal_buf[8+3];
   char decimal_buf_swapped_endianess[20];
 
-  printf("%d\n", readValue);
+  int n_two_complement = store_two_complement(two_complement_buf, readValue);
+  write(STDOUT_FD, two_complement_buf, n_two_complement);
+
+  int n_decimal = store_decimal(decimal_buf, readValue);
+  write(STDOUT_FD, decimal_buf, n_decimal);
+
+  int n_hexadecimal = store_hexadecimal(hexadecimal_buf, two_complement_buf, n_two_complement);
+  write(STDOUT_FD, hexadecimal_buf, n_hexadecimal);
+
+  int n_swapped_endianess = store_swap_endianess(decimal_buf_swapped_endianess, two_complement_buf, n_two_complement);
+  write(STDOUT_FD, decimal_buf_swapped_endianess, n_swapped_endianess);
 
 
   return 0;
