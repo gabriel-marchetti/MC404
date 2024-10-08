d77df49959158c27f640fa0eb8cede77c0890bcf
FouTY <g251055@dac.unicamp.br>
2024-10-08 16:44:19 -0300

    Completei o lab9.s

--- Search_Linked_List/lab9.s
+++ Search_Linked_List/lab9.s
@@ -2,6 +2,9 @@
 buffer: .skip 0x10              # it will be enough for containing the input.
 write_buffer: .skip 0x10        # it will store the output.
 
+.section .bss
+aux_stack: .space 16            #this stack will hold value for decimal
+
 .section .text
 .align 2
 .global _start
@@ -32,12 +35,11 @@ _start:
     end_next_node:
 
     la a0, write_buffer
-    mv a1, s1 
+    mv a1, t1
     jal store_decimal_to_buffer # a0 will hold buffer size.
     
-    debug:
     mv a2, a0
-    la a0, write_buffer
+    la a1, write_buffer
     jal write                   # write to stdout.
 
     jal exit
@@ -56,19 +58,39 @@ store_decimal_to_buffer:
     mv t2, a1
 
     bgez t2, positive_case
-        neg t2, t2
+        neg t2, t2              # transform t2 into positive.
         li  t6, '-'
         sb  t6, 0(a0)           # store negative sign.
         addi t0, t0, 1          # increment index.
     positive_case:
+
+    la t5, aux_stack
+    li t6, 0                        # size of the stack
     convert_loop:
         rem t4, t2, t1              # get remainder division by 10.
         addi t4, t4, '0'            # adjust to ASCII.
-        add t5, a0, t0              # correct buffer address.
+        addi t5, t5, 1              # increase stack.
         sb t4, 0(t5)                
+        addi t6, t6, 1              # increase size of stack
         div t2, t2, t1              # divide by 10
-        addi t0, t0, 1
         bnez t2, convert_loop
+    # add newline char at the end of the buffer.
+    debug: 
+    reverse_copy_stack:
+        beq t6, zero, end_reverse_copy_stack
+        add t3, a0, t0              # address of buffer.
+        lb t4, 0(t5)
+        sb t4, 0(t3)
+        addi t0, t0, 1              # incrementa buffer.
+        addi t5, t5, -1
+        addi t6, t6, -1
+        j reverse_copy_stack
+    end_reverse_copy_stack:
+
+    add t5, a0, t0
+    li t4, '\n'
+    sb t4, 0(t5)
+    addi t0, t0, 1 
     mv a0, t0
     ret
 ################################################################################
